123ce68ce3d5e722ac52263b13513c8c943ec6ea
daniel05lucena <daniellucenadc@gmail.com>
2024-02-09 01:10:53 -0400

    pagina web

---
+++ index.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+  $('#carrusel').slick({
+    // Opciones de Slick Carousel
+    dots: true,
+    infinite: true,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 640,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
+});
